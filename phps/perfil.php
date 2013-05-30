e4924b8ea98a974d5f7a7dad9b983390f271dfb8
cristian4 <criveragonzalez4@gmail.com>
2013-05-30 17:44:43 +0200

    Mostrar datos perfil
    El archivo perfil.php ya tiene una consulta y muestra los datos del
    usuario. Habrá que meter los datos en distintos inputs por si quiere
    modificar

--- phps/perfil.php
+++ phps/perfil.php
@@ -4,6 +4,7 @@
 		<title>Foro Starcraft II</title>
 		<link href="../imagenes/starcraft-logo.png" rel="shortcut icon" />
 		<link rel="stylesheet" type="text/css" href="../CSS/estilos.css" />
+		<link rel="stylesheet" type="text/css" href="../CSS/estilosPerfil.css" />
 		<script src="../JS/jquery.js"></script>
 		<?php
 			include_once "config.php";
@@ -14,15 +15,31 @@
 		<div id="contenedor">
 			<div id="foros">
 				<?php
-					$consulta= "SELECT * FROM datos_usuario WHERE idUsuario=(SELECT idUsuario from usuario where nickname='".$_SESSION['usuario']."');";
+					$consulta="SELECT E.nickname, D.nombre, D.correo, D.direccion, D.cod_postal, D.sexo, D.f_alta, D.f_nacimiento,E.rol FROM usuario E, datos_usuario D WHERE E.id=D.idUsuario=(SELECT idUsuario from usuario where nickname='".$_SESSION['usuario']."');";
 					$resultado=mysql_query($consulta) or
 					die("No se puede obtener tu información personal en este momento");
-					$i=0;
+
+					echo "<div id='texto'>";
+						echo "<label for='nick'>Nombre de usuario: </label><br />";
+						echo "<label for='nombre'>Nombre: </label><br />";
+						echo "<label for='email'>E-mail: </label><br />";
+						echo "<label for='direc'>Direccion: </label><br />";
+						echo "<label for='cPostal'>Codigo Postal: </label><br />";
+						echo "<label for='sexo'>Sexo: </label><br />";
+						echo "<label for='fRegistro'>Fecha de registro: </label><br />";
+						echo "<label for='fNacimiento'>Fecha de nacimiento: </label><br />";
+						echo "<label for='Permisos'>Permisos: </label><br />";
+					echo "</div>";
+
+
+					echo "<div id='input'>";
 					while($fila=mysql_fetch_array($resultado)){
-						for($i=2;$i<9;$i++){
+						for($i=0;$i<9;$i++){
 							echo $fila[$i]."<br />";
 						}
 					}
+					echo "</div>";
+			
 				?>
 			</div>
 		</div>
